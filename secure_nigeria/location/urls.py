816326c205f124303a7213c5c4f9a3f2f01406e8
adebisi_olayinka <adebisio024@gmail.com>
2025-12-27 14:25:13 +0100

    implement the urls for verify and unverify

--- secure_nigeria/location/urls.py
+++ secure_nigeria/location/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import LocationViewSet,StationViewSet,RiskViewSet,FeedViewSet,CommentViewset
+from .views import LocationViewSet,StationViewSet,RiskViewSet,FeedViewSet,CommentViewset,VerifyView,UnverifyView
 
 router=DefaultRouter()
 router.register(r'location',LocationViewSet)
@@ -11,4 +11,6 @@ router.register(r'comment',CommentViewset)
 
 urlpatterns=[
     path('',include(router.urls)),
+    path('feed/verify/<int:pk>/',VerifyView.as_view()),
+    path('feed/unverify/<int:pk>/',UnverifyView.as_view()),
 ]
