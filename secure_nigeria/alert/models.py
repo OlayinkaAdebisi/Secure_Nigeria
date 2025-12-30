aafc70f4c90834072c57d925b980ab984ad811fd
adebisi_olayinka <adebisio024@gmail.com>
2025-12-30 15:11:17 +0100

    added related name to differentiate between recienpt and actor

--- secure_nigeria/alert/models.py
+++ secure_nigeria/alert/models.py
@@ -4,8 +4,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 class Notification(models.Model):
-    recipient=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    actor=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    recipient=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='notifications')
+    actor=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='notification_actor')
     verb=models.CharField(max_length=256)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
