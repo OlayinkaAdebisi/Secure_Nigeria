bb372cefd9de2269ea77cbf185a8a6333cf4b432
adebisi_olayinka <adebisio024@gmail.com>
2025-12-30 15:06:15 +0100

    added the profile,follow and unfollow urls

--- secure_nigeria/accounts/urls.py
+++ secure_nigeria/accounts/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from .views import SignUpView,LoginView
+from django.urls import path,include
+from .views import SignUpView,LoginView,ProfileView,FollowView,UnfollowView
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register(r'profile',ProfileView)
 
 urlpatterns=[
+    path('',include(router.urls)),
     path("signup/",SignUpView.as_view()),
-    path("login/",LoginView.as_view())
+    path("login/",LoginView.as_view()),
+    path("follow/<int:pk>/",FollowView.as_view()),
+    path("unfollow/<int:pk>/",UnfollowView.as_view()),
 ]
